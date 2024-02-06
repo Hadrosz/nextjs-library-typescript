c4ac99fa201f62b3a99dd5b8ae9f2c492e417832
Alejadro Vasquez <Alejandro.vasquezes17@gmail.com>
2024-02-05 23:14:51 -0500

    .

--- src/app/components/ListCardBook.tsx
+++ src/app/components/ListCardBook.tsx
@@ -2,5 +2,5 @@ import { Book } from "@/app/types/Book";
 import CardBook from "./CardBooks";
 
 export default function ListCardBook({ book }: { book: Book }) {
-  return <CardBook book={book} />;
+  return <CardBook book={book} styles={{ show: true }} />;
 }
