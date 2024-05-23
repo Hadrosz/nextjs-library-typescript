1af732500cd1b89b4f9e35622323de1c224f1dca
Alejadro Vasquez <Alejandro.vasquezes17@gmail.com>
2024-05-22 23:00:03 -0500

    video nea

---
+++ src/app/videos/video.jsx
@@ -0,0 +1,10 @@
+import { ThemeSwitcher } from "@/components/client-components/ThemeSwitcher";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+
+export default function video() {
+  return (
+    <video>
+      <source src="/2024_NeaStudio_Reel_3D_v01.mp4" type="video/mp4" />
+    </video>
+  );
+}
